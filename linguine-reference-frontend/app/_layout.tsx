56653200cc4d04aae1083edf29fe3e6d9ac67d78
p-febis <preston.febis@yahoo.com>
2025-02-27 19:11:34 -0300

    Started work on refrence frontend

---
+++ linguine-reference-frontend/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function RootLayout() {
+  return (
+    <React.StrictMode>
+      <Stack />
+    </React.StrictMode>
+  );
+}
---
+++ linguine-reference-frontend/app/lib/linguine.ts
@@ -0,0 +1,4 @@
+import * as Linguine from "@linguine/client-sdk";
+
+export const linguineClient = new Linguine.Client({ baseUrl: process.env.EXPO_PUBLIC_LINGUINE_API ?? "http://localhost:3000" })
+export const linguineAuthClient = new Linguine.AuthClient(linguineClient, { accessTokenStorage: new Map() })
