c7079e98945de374ce1bcb9249d08db465fa98dc
p-febis <preston.febis@yahoo.com>
2025-02-28 17:35:36 -0300

    Enforce code formatting in the refrence frontned

--- refrence-frontend/app/lib/linguine.ts
+++ refrence-frontend/app/lib/linguine.ts
@@ -1,4 +1,8 @@
 import * as Linguine from "@linguine/client-sdk";
 
-export const linguineClient = new Linguine.Client({ baseUrl: process.env.EXPO_PUBLIC_LINGUINE_API ?? "http://localhost:3000" })
-export const linguineAuthClient = new Linguine.AuthClient(linguineClient, { accessTokenStorage: new Map() })
+export const linguineClient = new Linguine.Client({
+  baseUrl: process.env.EXPO_PUBLIC_LINGUINE_API ?? "http://localhost:3000",
+});
+export const linguineAuthClient = new Linguine.AuthClient(linguineClient, {
+  accessTokenStorage: new Map(),
+});
